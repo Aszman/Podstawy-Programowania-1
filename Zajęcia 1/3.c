418d6278e5f56375d063de37eab4b261746f35d8
Aszman <72223520+Aszman@users.noreply.github.com>
2020-10-08 14:36:38 +0200

    Create 3.c

---
+++ Zajęcia 1/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char im[50];
+    char naz[50];
+    
+    printf("Wpisz imię i nazwisko: ");
+    scanf("%49s %49s", im, naz);
+    printf("%s %s\n", naz, im);
+    
+    return 0;
+    
+}
