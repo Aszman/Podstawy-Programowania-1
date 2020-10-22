acddc2fb6ac9283f97bf7669ef51b4b6800d0ebc
Łukasz Starzyk <72223520+Aszman@users.noreply.github.com>
2020-10-22 14:53:45 +0200

    Create zad1.c

---
+++ Zajęcia 4 (22.10.2020)/zad1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    for(int i=10; i>=2;--i)
+    {
+        if (i%2==0)
+        {
+            printf("%d\n", i);    /* do repo */
+        }
+    }
+        
+    return 0;
+}
