6c65594c51a43f9f0b7f25dd7f373f985e05c961
Łukasz Starzyk <72223520+Aszman@users.noreply.github.com>
2020-10-29 14:30:07 +0100

    Create 2.c

---
+++ Zajęcia 5 (29.10.2020)/2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void zlozenie(char *a, char *b)
+{
+    static int count = 1;
+    printf("%i: %s %s\n",count,a, b); /* do repo*/
+    ++count;
+}
+
+int main()
+{
+    zlozenie("error","komunikat");
+    zlozenie("informacja","komunikat");
+    
+    return 0;
+}
