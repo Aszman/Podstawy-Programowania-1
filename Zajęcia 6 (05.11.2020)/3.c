f32e184530e259ee8a554865963bed5ee17a596f
Łukasz Starzyk <72223520+Aszman@users.noreply.github.com>
2020-11-05 15:02:57 +0100

    Create 3.c

---
+++ Zajęcia 6 (05.11.2020)/3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ddouble(int size, int *dynTablica)
+{
+    dynTablica = realloc(dynTablica, 2*size * sizeof(*dynTablica));   //do repo
+    
+    for(int i = size; i < 2*size; ++i)
+    {
+        dynTablica[i] = dynTablica[i%size];
+    }
+}
+
+int main()
+{
+    int tab[] = {4,8,2,8,35,7};
+    const int size = sizeof(tab)/sizeof(tab[0]);
+        
+    int *dynTablica = malloc(size * sizeof(tab));
+    
+    for(int i = 0; i < size; ++i)
+    {
+        dynTablica[i] = tab[i];
+    }
+    
+    ddouble(size, dynTablica);
+    
+    for(int i =0; i < 2*size; ++i)
+    {
+        printf("%d ",dynTablica[i]);
+    }
+    
+    free(dynTablica);
+    
+    return 0;
+}
